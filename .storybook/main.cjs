dbb70635d6349a8ae4b0f71b14b3ee90fa9fefef
Henrique de Castro <hendecastro@gmail.com>
2023-03-02 00:27:02 -0300

    chore: small changes

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
     "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
@@ -15,9 +15,9 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configTipe }) => {
-    if (configTipe === 'PRODUCTION') {
-      config.base = '/ui-kit'
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ui-kit/'
     }
 
     return config
